28aed27d955a51f4cda659d8c1b4c86d0991b7ae
mcgillglasswater152 <128558637+mcgillglasswater152@users.noreply.github.com>
2024-09-08 04:24:13 -0600

    merge to github from Corsten

--- sync_github.py
+++ sync_github.py
@@ -1,17 +1,20 @@
 import os
 from platform import uname
-#kill your self
+
 os.chdir(os.path.dirname(__file__))
 
 message = f'merge to github from {uname().node}'
+
 
 def push(message):
     os.system('git add .')
     os.system(f'git commit -m "{message}"')
     os.system('git push -u origin master')
 
+
 def pull():
     os.system('git pull origin master')
+
 
 def init(repo_url):
     os.system('git init')
@@ -20,9 +23,16 @@ def init(repo_url):
     os.system(f'git remote add origin {repo_url}.git')
     os.system('git push -u origin master')
 
+
 def push_pull(message):
     push(message)
     pull()
 
+
+def set_to_merge():
+    os.system('git config pull.rebase false')
+
+
+set_to_merge()
 for i in range(3):
     push_pull(message)
